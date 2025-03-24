1df6dfc40ccd3ca5fccc0d9668fd5034a7843c9a
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2025-03-24 11:30:38 +0700

    feat(CrowdFund) : add new function for updated Campaign by userAddress n campaignId

--- contracts/CrowdFund.sol
+++ contracts/CrowdFund.sol
@@ -10,17 +10,26 @@ interface IUser {
         string email;
     }
 
-    function getUser(address _address) external view returns (UserStruct memory);
+    function getUser(
+        address _address
+    ) external view returns (UserStruct memory);
 }
 
 contract CrowdFund is Ownable {
-
     // INTERFACE
     IUser public userContract;
 
     // EVENT
     event CampaignCreated(
         address indexed creator,
+        uint indexed campaignId,
+        string _nameCampaign,
+        string _description,
+        uint _targetFund,
+        uint _deadline
+    );
+    
+    event CampaignUpdated(
         uint indexed campaignId,
         string _nameCampaign,
         string _description,
@@ -62,7 +71,6 @@ contract CrowdFund is Ownable {
         uint _targetFund,
         uint _deadline
     ) public onlyRegister {
-
         require(_deadline > block.timestamp, "Deadline must be in the future");
 
         uint campaignId = crowdFunds[msg.sender].length + 1;
@@ -79,7 +87,56 @@ contract CrowdFund is Ownable {
 
         crowdFunds[msg.sender].push(newCampaign);
 
-        emit CampaignCreated(msg.sender, campaignId,_nameCampaign, _description, _targetFund, _deadline);
+        emit CampaignCreated(
+            msg.sender,
+            campaignId,
+            _nameCampaign,
+            _description,
+            _targetFund,
+            _deadline
+        );
     }
 
+    // FUNCTION UPDATE CAMPAIGN
+    function updateCampaign(
+        uint _campaignId,
+        string memory _nameCampaign,
+        string memory _description,
+        uint _targetFund,
+        uint _deadline
+    ) public onlyRegister {
+        require(_campaignId > 0, "Parameter Campaign ID not found!!");
+
+        CrowdFundStruct[] storage campaigns = crowdFunds[msg.sender];
+
+        bool campaignFound = false;
+        for (uint256 index = 0; index < campaigns.length; index++) {
+            if (campaigns[index].campaignId == _campaignId) {
+                if (bytes(_nameCampaign).length > 0) {
+                    campaigns[index].nameCampaign = _nameCampaign;
+                }
+                if (bytes(_description).length > 0) {
+                    campaigns[index].description = _description;
+                }
+                if (_targetFund > 0) {
+                    campaigns[index].targetFund = _targetFund;
+                }
+                if (_deadline > 0) {
+                    campaigns[index].deadline = _deadline;
+                }
+                campaignFound = true;
+                break;
+            }
+        }
+
+        require(campaignFound, "Campaign with this ID does not exist!");
+
+        emit CampaignUpdated(
+            _campaignId,
+            _nameCampaign,
+            _description,
+            _targetFund,
+            _deadline
+        );
+    }
 }
--- test/CrowdFund.test.ts
+++ test/CrowdFund.test.ts
@@ -12,19 +12,20 @@ describe('CrowdFund', function () {
     let userContract: User;
     let owner: HardhatEthersSigner;
     let user: HardhatEthersSigner;
+    let user2: HardhatEthersSigner;
 
     before(async function () {
 
         // Deploy the User contract
         const User = await ethers.getContractFactory("User");
         userContract = await User.deploy();
-
+        
         // Deploy the CrowdFund contract
         const CrowdFund = await ethers.getContractFactory('CrowdFund');
         crowdFund = await CrowdFund.deploy(userContract.getAddress());
 
         // Get signers
-        [owner, user] = await ethers.getSigners();
+        [owner, user, user2] = await ethers.getSigners();
     });
 
     it("Should create a new Campaign", async () => {
@@ -43,7 +44,7 @@ describe('CrowdFund', function () {
             crowdFund.connect(user).createCampaign(nameCampaign, description, targetFund, deadline)
         ).to.emit(crowdFund, "CampaignCreated") // apakah memicu event CampaignCreated
             .withArgs(user.address, 1, nameCampaign, description, targetFund, deadline)
-            // cek apakah argumen yang diterima sesuai dengan args nya
+        // cek apakah argumen yang diterima sesuai dengan args nya
 
         // Verify campaign details
         const campaign = await crowdFund.crowdFunds(user.address, 0);
@@ -54,5 +55,55 @@ describe('CrowdFund', function () {
         expect(campaign.targetFund).to.equal(targetFund);
         expect(campaign.currentFund).to.equal(0);
         expect(campaign.deadline).to.equal(deadline);
-    })
+    });
+
+    it("Should update campaign details successfully", async () => {
+
+        const campaignId = 1;
+        const newNameCampaign = 'New Updated Name';
+        const newDescription = 'New Updated Description';
+        const newTargetFund = ethers.parseEther("2");
+        const newDeadline = Math.floor(Date.now() / 1000) + 86400;
+
+        await expect(
+            crowdFund.connect(user).updateCampaign(campaignId, newNameCampaign, newDescription, newTargetFund, newDeadline)
+        ).to.emit(crowdFund, "CampaignUpdated")
+            .withArgs(campaignId, newNameCampaign, newDescription, newTargetFund, newDeadline)
+
+        const campaign = await crowdFund.crowdFunds(user.address, 0);
+        expect(campaign.creator).to.equal(user.address);
+        expect(campaign.campaignId).to.equal(campaignId);
+        expect(campaign.nameCampaign).to.equal(newNameCampaign);
+        expect(campaign.description).to.equal(newDescription);
+        expect(campaign.targetFund).to.equal(newTargetFund);
+        expect(campaign.deadline).to.equal(newDeadline);
+    });
+
+    it("should only update non-empty fields", async function () {
+
+        const campaignId = 1;
+        const newName = ""; // Kosong, tidak update
+        const newDescription = "Partially Updated";
+        const newTargetFund = 0; // Nol, tidak update
+        const newDeadline = 1700000000;
+
+        await expect(
+            crowdFund.connect(user).updateCampaign(campaignId, newName, newDescription, newTargetFund, newDeadline)
+        ).to.emit(crowdFund, "CampaignUpdated")
+            .withArgs(
+                campaignId,
+                newName,
+                newDescription,
+                newTargetFund,
+                newDeadline
+            );
+
+        const campaign = await crowdFund.crowdFunds(user.address, 0);
+        expect(campaign.creator).to.equal(user.address);
+        expect(campaign.campaignId).to.equal(campaignId);
+        expect(campaign.description).to.equal(newDescription);
+        expect(campaign.deadline).to.equal(newDeadline);
+
+    });
+
 });
